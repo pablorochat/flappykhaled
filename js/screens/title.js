f552fa74f0cc41c4c3280cd2709b23bf5a10269a
pablorochat <pabloorochat@gmail.com>
2015-12-21 16:44:54 -0800

    new background and start copy

--- js/screens/title.js
+++ js/screens/title.js
@@ -48,7 +48,7 @@ game.TitleScreen = me.ScreenObject.extend({
                 // size does not matter, it's just to avoid having a zero size
                 // renderable
                 this._super(me.Renderable, 'init', [0, 0, 100, 100]);
-                this.text = me.device.touch ? 'Tap to start' : 'PRESS SPACE OR CLICK LEFT MOUSE BUTTON TO START \n\t\t\t\t\t\t\t\t\t\t\tPRESS "M" TO MUTE SOUND';
+                this.text = me.device.touch ? 'Tap to start' : 'USE SPACEBAR OR CLICK';
                 this.font = new me.Font('gamefont', 20, '#FFFF00');
             },
             draw: function (renderer) {
